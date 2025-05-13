f2d53f3a4cd9bd16e5f300d599de3cc9217c9581
Dan <101811313+D4NGK4@users.noreply.github.com>
2025-05-13 08:16:01 +0800

    Fix syntax error in read_root function and change to async

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return{"message: Hello, FastAPI!"}
+async def read_root():
+    return {"message": "Hello, FastAPI!"}
 
 @app.get("/greet/{name}")
 def greet(name: str):
